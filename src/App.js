2d42816e1e734c563f31eed2c09f0699ccdd9d4c
Matshelane Rapolai <matshelanerapolai@gmail.com>
2024-10-08 17:14:15 +0200

    added a rel noreferrer on line 9

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
     <h1>Weather App</h1>  
     <footer>
-      This project was coded by <a href="https://github.com/MMRapolai/" target="_blank">Matshelane Rapolai</a> and is <a href="https://github.com/MMRapolai/murielweek5-react" target="_blank"> open-sourced on GitHub </a>
+      This project was coded by <a href="https://github.com/MMRapolai/" target="_blank" rel="noreferrer">Matshelane Rapolai</a> and is <a href="https://github.com/MMRapolai/murielweek5-react" target="_blank" rel="noreferrer"> open-sourced on GitHub </a>
     </footer>
     </div>
   );
