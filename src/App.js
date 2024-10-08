8eb9eb9c2e34db46e7b01f86c3998f8ce047bcbb
Matshelane Rapolai <matshelanerapolai@gmail.com>
2024-10-08 20:12:16 +0200

    Created a component weather js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Weather from "./weather";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-    <h1>Weather App</h1>  
+    <h1>Weather App</h1> 
+    <Weather/> 
     <footer>
       This project was coded by <a href="https://github.com/MMRapolai/" target="_blank" rel="noreferrer">Matshelane Rapolai</a> and is <a href="https://github.com/MMRapolai/murielweek5-react" target="_blank" rel="noreferrer"> open-sourced on GitHub </a>
     </footer>
---
+++ src/weather.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./weather.css";
+
+export default function weather() {
+    return (
+    <div className="weather">
+        Hello from weather
+    </div>
+    )
+    ;}
