aba25dde5dfc0987d8d118275a85ed7a85e90be1
Matshelane Rapolai <matshelanerapolai@gmail.com>
2024-10-08 17:04:30 +0200

    Added o footer with links

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-    Weather App  
+    <h1>Weather App</h1>  
+    <footer>
+      This project was coded by <a href="https://github.com/MMRapolai/" target="_blank">Matshelane Rapolai</a> and is <a href="https://github.com/MMRapolai/murielweek5-react" target="_blank"> open-sourced on GitHub </a>
+    </footer>
     </div>
   );
 }
